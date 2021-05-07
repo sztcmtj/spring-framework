5b1b40c011faf47175c84a0c4a162da16763ea5b
mutianjie <mtj_313@163.com>
2021-05-07 21:12:22 +0800

    2021-05-07

--- spring-learn/src/main/java/com/ustc/learn/DemoApplication.java
+++ spring-learn/src/main/java/com/ustc/learn/DemoApplication.java
@@ -1,15 +1,9 @@
 package com.ustc.learn;
 
-import com.ustc.learn.aspect.AspectJTest;
 import com.ustc.learn.beans.TestBean;
-import com.ustc.learn.beans.TestBeanJdk;
-import com.ustc.learn.impl.UserServiceImpl;
-import com.ustc.learn.proxy.MyInvocationHandler;
 import com.ustc.learn.services.UserService;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.lang.reflect.Proxy;
 
 public class DemoApplication {
 	public static void main(String[] args) {
--- spring-learn/src/main/java/com/ustc/learn/beans/CircularBeanB.java
+++
@@ -1,9 +0,0 @@
-package com.ustc.learn.beans;
-
-public class CircularBeanB {
-	private CircularBeanA a;
-
-	public void setA(CircularBeanA a) {
-		this.a = a;
-	}
-}
